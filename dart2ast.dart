a78032da50b5921e534127c32a01fcbae3b9cb6a
Kevin Millikin <kmillikin@chromium.org>
2014-06-06 10:39:00 +0200

    Drastically simplify parsing from Dart to S-expressions.
    Instead of trying to build a string in one pass, construct an explicit
    S-expression in Dart and then build a string from that.  Since it is
    simple to do so, pretty print the S-expression.
    
    Pretty printing uses Paul Brauner's pretty package:
    http://pub.dartlang.org/packages/pretty

--- dart2ast.dart
+++ dart2ast.dart
@@ -3,6 +3,7 @@
 import 'dart:io';
 
 import 'package:analyzer/analyzer.dart' as ast;
+import 'package:pretty/pretty.dart' as pretty;
 
 main(List<String> args) {
   if (args.length != 1) {
@@ -10,215 +11,221 @@ main(List<String> args) {
     exit(0);
   }
 
-  String path = args[0];
+  String path = args.first;
   ast.CompilationUnit compilationUnit = ast.parseDartFile(path);
-  _AstPrintingVisitor visitor = new _AstPrintingVisitor();
-  compilationUnit.accept(visitor);
-  print(visitor.buffer);
+  _Ast2SexpVisitor visitor = new _Ast2SexpVisitor();
+  print(_sexp2Doc(compilationUnit.accept(visitor)).render(120));
 }
 
-class _AstPrintingVisitor extends ast.GeneralizingAstVisitor {
-  StringBuffer buffer = new StringBuffer();
 
+_sexp2Doc(expression) {
+  if (expression is String) return pretty.text(expression);
+  if ((expression as List).isEmpty) return pretty.text('()');
+
+  Iterable<pretty.Document> docs = (expression as List).map(_sexp2Doc);
+  pretty.Document result = pretty.text('(') + docs.first;
+  pretty.Document children =
+      docs.skip(1).fold(pretty.empty,
+          (doc0, doc1) => doc0 + pretty.line + doc1);
+  result += (children + pretty.text(')')).nest(2);
+  return result.group;
+}
+
+
+/// Translate a compilation unit AST to a list of S-expressions.
+///
+/// An S-expression is an atom (string in this case) or a list of
+/// S-expressions.
+class _Ast2SexpVisitor extends ast.GeneralizingAstVisitor {
   giveup(String why) {
     print('Unsupported syntax: $why.');
     exit(0);
   }
+
+  visit(ast.AstNode node) => node.accept(this);
 
   visitNode(ast.AstNode node) {
     giveup(node.runtimeType.toString());
   }
 
   visitCompilationUnit(ast.CompilationUnit node) {
-    for (ast.AstNode d in node.sortedDirectivesAndDeclarations) {
-      d.accept(this);
-    }
+    return node.sortedDirectivesAndDeclarations.map(visit)
+        .toList(growable: false);
   }
 
   visitFunctionDeclaration(ast.FunctionDeclaration node) {
     if (node.name == null) giveup('unnamed function declaration');
 
-    // ==== Tag ====
+    // ==== Tag and name ====
+    String tag;
     String name = node.name.name;
     if (name.contains('_async')) {
-      buffer.write('(Async ');
+      tag = 'Async';
     } else if (name.contains('_syncStar')) {
-      buffer.write('(SyncStar ');
+      tag = 'SyncStar';
     } else {
-      buffer.write('(Sync ');
+      tag = 'Sync';
     }
-
-    // ==== Name ====
-    buffer.write(name);
 
     // ==== Parameter list ====
-    buffer.write('(');
     ast.FunctionExpression function = node.functionExpression;
-    bool first = true;
-    for (ast.FormalParameter p in function.parameters.parameters){
-      if (!first) buffer.write(' ');
-      first = false;
-      p.accept(this);
-    }
-    buffer.write(')');
+    List parameters = function.parameters.parameters.map(visit)
+        .toList(growable: false);
 
     // ==== Local variable list ====
-    // Here we assume that all variables have been hoisted to the top of the
-    // function and declared (but not initialized) in a single statement.
-    // We allow there to be no such declaration.
-    buffer.write('(');
+    // Here it is assumed that all variables have been hoisted to the top of
+    // the function and declared (but not initialized) in a single statement.
+    // There is allowed to be no declaration.
     if (function.body is! ast.BlockFunctionBody) {
       giveup('not a block function');
     }
-    ast.BlockFunctionBody body = function.body;
-    ast.Block block = body.block;
-    if (!block.statements.isEmpty
-        && block.statements[0] is ast.VariableDeclarationStatement) {
-      ast.VariableDeclarationStatement decl = block.statements[0];
-      bool first = true;
-      for (ast.VariableDeclaration d in decl.variables.variables){
-        if (!first) buffer.write(' ');
-        first = false;
-        d.accept(this);
-      }
+    ast.Block block = (function.body as ast.BlockFunctionBody).block;
+    List locals;
+    if (block.statements.isEmpty
+        || block.statements.first is! ast.VariableDeclarationStatement) {
+      locals = [];
+    } else {
+      locals = (block.statements.first as ast.VariableDeclarationStatement)
+          .variables.variables.map(visit).toList(growable: false);
     }
-    buffer.write(')');
 
-    first = true;
-    for (ast.Statement s in block.statements){
+    bool first = true;
+    List body, current;
+    for (ast.Statement s in block.statements) {
       if (first && s is ast.VariableDeclarationStatement) {
+        // Skip an initial variable declaration statement.
         first = false;
         continue;
       }
       first = false;
-      s.accept(this);
+      if (current == null) {
+        current = body = visit(s);
+      } else {
+        current = current[current.length - 1] = visit(s);
+      }
     }
 
-    buffer.write(')');
+    return [tag, name, parameters, locals, body];
   }
 
   visitSimpleFormalParameter(ast.SimpleFormalParameter node) {
-    buffer.write(node.identifier.name);
+    return node.identifier.name;
   }
 
   visitVariableDeclaration(ast.VariableDeclaration node) {
-    buffer.write(node.name.name);
+    return node.name.name;
   }
 
   // ==== Expressions ====
   visitIntegerLiteral(ast.IntegerLiteral node) {
-    buffer.write('(Constant ${node.value})');
+    return ['Constant', node.value.toString()];
   }
 
   visitSimpleIdentifier(ast.SimpleIdentifier node) {
-    buffer.write('(Variable ${node.name})');
+    return ['Variable', node.name];
   }
 
   visitAssignmentExpression(ast.AssignmentExpression node) {
-    buffer.write('(Assignment');
-    node.leftHandSide.accept(this);
-    node.rightHandSide.accept(this);
-    buffer.write(')');
+    return ['Assignment', visit(node.leftHandSide),
+            visit(node.rightHandSide)];
   }
 
   visitMethodInvocation(ast.MethodInvocation node) {
     if (node.target != null) giveup("method with a receiver");
 
-    // We don't check that yield and yield* occur as statements.
+    // It is not checked that yield and yield* occur as statements.
     String name = node.methodName.name;
     ast.NodeList<ast.Expression> arguments = node.argumentList.arguments;
-    String recognizedName;
+    String tag;
     if (name == 'await') {
-      recognizedName = 'Await';
+      tag = 'Await';
     } else if (name == 'yield') {
-      recognizedName = 'Yield';
+      tag = 'Yield';
     } else if (name == 'yield_star') {
-      recognizedName = 'YieldStar';
+      tag = 'YieldStar';
     }
 
-    if (recognizedName != null) {
-      buffer.write('($recognizedName');
+    if (tag != null) {
       if (arguments.length != 1) giveup('wrong arity for $name');
-      arguments[0].accept(this);
+      return [tag, visit(arguments.first)];
     } else {
-      buffer.write('(Call ${node.methodName.name}');
-      buffer.write('(');
-      for (ast.Expression e in node.argumentList.arguments){
-        e.accept(this);
-      }
-      buffer.write(')');
+      List arguments = node.argumentList.arguments.map(visit)
+          .toList(growable: false);
+      return ['Call', name, arguments];
     }
-    buffer.write(')');
   }
 
   visitThrowExpression(ast.ThrowExpression node) {
-    buffer.write('(Throw');
-    node.expression.accept(this);
-    buffer.write(')');
+    return ['Throw', visit(node.expression)];
   }
 
   // ==== Statements ====
   visitBlock(ast.Block node) {
-    buffer.write('(Block');
-    for (ast.Statement s in node.statements){
-      s.accept(this);
+    // Blocks can occur as bodies in if/else, while, try/catch/finally, etc.
+    // Since statements in the output are really linked lists of statements
+    // terminated by NoStatement, blocks are 'flattened'.
+    List result, current;
+    for (ast.Statement s in node.statements) {
+      if (current == null) {
+        current = result = visit(s);
+      } else {
+        current = current[current.length - 1] = visit(s);
+      }
     }
-    buffer.write(')');
+    return result;
   }
 
   visitExpressionStatement(ast.ExpressionStatement node) {
-    buffer.write('(Expression');
-    node.expression.accept(this);
-    buffer.write(')');
+    return ['Expression', visit(node.expression), 'NoStatement'];
   }
 
   visitReturnStatement(ast.ReturnStatement node) {
-    // We require a subexpression for return, except for a return from a
+    // A subexpression is required for return, except for a return from a
     // sync* function, which should have no subexpression.
-    if (node.expression == null) {
-      buffer.write('(YieldBreak');
-    } else {
-      buffer.write('(Return');
-      node.expression.accept(this);
-    }
-    buffer.write(')');
+    return node.expression == null
+        ? ['YieldBreak', 'NoStatement']
+        : ['Return', visit(node.expression), 'NoStatement'];
   }
 
   visitIfStatement(ast.IfStatement node) {
     if (node.elseStatement == null) giveup('if without an else');
-    buffer.write('(If');
-    node.condition.accept(this);
-    node.thenStatement.accept(this);
-    node.elseStatement.accept(this);
-    buffer.write(')');
+    return ['If', visit(node.condition), visit(node.thenStatement),
+            visit(node.elseStatement), 'NoStatement'];
   }
 
   visitLabeledStatement(ast.LabeledStatement node) {
+    // Loops in the output are always labeled.  If the labeled statement is
+    // a loop then the label is attached to the loop.
     if (node.labels.length != 1) giveup('multiple labels');
-    buffer.write('(Label');
-    node.statement.accept(this);
-    buffer.write(')');
+    String label = node.labels.first.label.name;
+    List statement = visit(node.statement);
+    if (statement.first == 'While') {
+      statement[1] = label;
+      return statement;
+    } else {
+      return ['Label', label, statement, 'NoStatement'];
+    }
   }
 
   visitBreakStatement(ast.BreakStatement node) {
     if (node.label == null) giveup('break without a label');
-    buffer.write('(Break ${node.label.name})');
+    return ['Break', node.label.name, 'NoStatement'];
   }
 
   visitWhileStatement(ast.WhileStatement node) {
-    buffer.write('(While');
-    node.condition.accept(this);
-    node.body.accept(this);
-    buffer.write(')');
+    // There is a null placeholder for the statement's label.  It is filled
+    // in by the caller.
+    return ['While', null, visit(node.condition), visit(node.body),
+            'NoStatement'];
   }
 
   visitContinueStatement(ast.ContinueStatement node) {
     if (node.label == null) giveup('continue without a label');
-    buffer.write('(Continue ${node.label.name})');
+    return ['Continue', node.label.name, 'NoStatement'];
   }
 
   visitTryStatement(ast.TryStatement node) {
-    // We support try/catch and try/finally.  Try/catch/finally can be
+    // Try/catch and try/finally are supported.  Try/catch/finally can be
     // desugared into a language with only try/catch and try/finally:
     //
     // try { S0 } catch (e) { S1 } finally { S2 }
@@ -234,25 +241,15 @@ class _AstPrintingVisitor extends ast.GeneralizingAstVisitor {
     if (node.catchClauses.isEmpty) {
       // This is probably impossible:
       if (node.finallyBlock == null) giveup('try without catch or finally');
-      buffer.write('(TryFinally');
-      node.body.accept(this);
-      node.finallyBlock.accept(this);
+      return ['TryFinally', visit(node.body), visit(node.finallyBlock),
+              'NoStatement'];
     } else if (node.catchClauses.length == 1) {
       if (node.finallyBlock != null) giveup('try/catch/finally');
-      buffer.write('(TryCatch');
-      node.body.accept(this);
-      ast.CatchClause clause = node.catchClauses[0];
-      buffer.write(clause.exceptionParameter.name);
-      clause.body.accept(this);
+      ast.CatchClause clause = node.catchClauses.first;
+      return ['TryCatch', visit(node.body), clause.exceptionParameter.name,
+              visit(clause.body), 'NoStatement'];
     } else {
       giveup('multiple catch clauses');
     }
-    buffer.write(')');
-  }
-
-  handleNode(ast.AstNode node) {
-    buffer.write('(${node.runtimeType}');
-    node.visitChildren(this);
-    buffer.write(')');
   }
 }
