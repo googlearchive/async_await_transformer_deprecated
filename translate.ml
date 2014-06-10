548c97e06ed4e7f4f2b2edf66b105136c2c52ebb
Kevin Millikin <kmillikin@chromium.org>
2014-06-10 10:15:28 +0200

    Remove the argument from break and continuation continuations.
    Break and continuation continuations do not actually have to have the
    same signature as throw and return continuations.  They are not called
    directly from finally blocks, instead wrapper continuations are
    called.
    
    So: wrappers need to take an extra (ignored) arguments, actual
    non-wrapper break and continuation continuations do not.
    
    Also: add type annotations to translate.ml.

--- translate.ml
+++ translate.ml
@@ -1,18 +1,36 @@
+(* Name generation. *)
 let gensym_counter = ref 0
 
 let reset_gensym () = gensym_counter := 0
 
-let gensym base_name =
+let gensym (base_name: string): string =
   let id = base_name ^ string_of_int (!gensym_counter) in
   (gensym_counter := !gensym_counter + 1; id)
 
+
+(* The compile-time environment maps local variable names to names in the
+   intermediate representation. *)
+type environment = (string * string) list
+
+(* Initially map every name to itself. *)
 let initial_env parameters locals =
   List.map (fun x -> (x, x)) (parameters @ locals)
 
-let update_env x v env =
+(* Add a new binding. *)
+let update_env (x: string) (v: string) (env: environment): environment =
   List.map (fun (x', v') -> if x' = x then (x, v) else (x', v')) env
 
-let rec translate_e expr env k ek =
+
+(* The translation itself is written in CPS. *)
+type statement_cont = environment -> Ir.expression
+type expression_cont = string -> environment -> Ir.expression
+type exception_cont = string -> environment -> Ir.expression
+type jump_cont = environment -> Ir.expression
+type return_cont = string -> environment -> Ir.expression
+
+let rec translate_e (expr: Ast.expression) (env: environment)
+    (k: expression_cont)
+    (ek: exception_cont): Ir.expression =
   match expr with
     | Ast.Constant n ->
       let v = gensym "v" in
@@ -46,7 +64,9 @@ let rec translate_e expr env k ek =
               Ir.LetVal ("_", Ir.Await (v, c, throw),
                 Ir.CallCont ("return", ["_"])))))
         ek
-and translate_args args env k ek =
+and translate_args (args: Ast.expression list) (env: environment)
+    (k: string list -> environment -> Ir.expression)
+    (ek: exception_cont): Ir.expression =
   match args with
     | [] -> k [] env
     | arg :: args ->
@@ -77,7 +97,12 @@ let fresh_env env =
     env
     ([], [])
 
-let rec translate_s stmt env k ek bks cks rk =
+let rec translate_s (stmt: Ast.statement) (env: environment)
+    (k: statement_cont)
+    (ek: exception_cont)
+    (bks: (string * jump_cont) list)
+    (cks: (string * jump_cont) list)
+    (rk: return_cont): Ir.expression =
   match stmt with
     | Ast.NoStatement ->
       k env
@@ -96,8 +121,8 @@ let rec translate_s stmt env k ek bks cks rk =
         (fun current env ->
           Ir.LetVal (moveNext, Ir.Fun ([], "return", "throw",
                                        translate_s next env k ek bks cks rk),
-            Ir.LetVal (v, Ir.Single (current, moveNext),
-              rk v env))) ek
+                     Ir.LetVal (v, Ir.Single (current, moveNext),
+                                rk v env))) ek
     | Ast.YieldStar (expr, next) ->
       let moveNext = gensym "moveNext" in
       let v = gensym "v" in
@@ -105,8 +130,8 @@ let rec translate_s stmt env k ek bks cks rk =
         (fun current env ->
           Ir.LetVal (moveNext, Ir.Fun ([], "return", "throw",
                                        translate_s next env k ek bks cks rk),
-            Ir.LetVal (v, Ir.Nested (current, moveNext),
-              rk v env))) ek
+                     Ir.LetVal (v, Ir.Nested (current, moveNext),
+                                rk v env))) ek
     | Ast.If (expr, thn, els, next) ->
       let join = gensym "join" in
       let (join_env, join_parameters) = fresh_env env in
@@ -115,17 +140,17 @@ let rec translate_s stmt env k ek bks cks rk =
       translate_e expr env
         (fun v env ->
 	  Ir.LetCont (join, join_parameters, translate_s next join_env k ek bks cks rk,
-            Ir.LetCont (if_true, [], translate_s thn env (mkcont join) ek bks cks rk,
-              Ir.LetCont (if_false, [], translate_s els env (mkcont join) ek bks cks rk,
-                Ir.If (v, if_true, if_false)))))
+                      Ir.LetCont (if_true, [], translate_s thn env (mkcont join) ek bks cks rk,
+                                  Ir.LetCont (if_false, [], translate_s els env (mkcont join) ek bks cks rk,
+                                              Ir.If (v, if_true, if_false)))))
         ek
     | Ast.Label (label, stmt, next) ->
       let break = gensym "break" in
       let (break_env, break_parameters) = fresh_env env in
-      Ir.LetCont (break, "_" :: break_parameters, translate_s next break_env k ek bks cks rk,
-        translate_s stmt env (mkcont1 break "_") ek ((label, mkcont1 break) :: bks) cks rk)
+      Ir.LetCont (break, break_parameters, translate_s next break_env k ek bks cks rk,
+                  translate_s stmt env (mkcont1 break "_") ek ((label, mkcont break) :: bks) cks rk)
     | Ast.Break (label, next) ->
-      List.assoc label bks "_" env
+      List.assoc label bks env
     | Ast.While (label, expr, stmt, next) ->
       let loop = gensym "continue" in
       let (loop_env, loop_parameters) = fresh_env env in
@@ -133,21 +158,21 @@ let rec translate_s stmt env k ek bks cks rk =
       let (break_env, break_parameters) = fresh_env env in
       let not_taken = gensym "not_taken" in
       let body = gensym "body" in
-      Ir.LetCont (loop, "_" :: loop_parameters,
+      Ir.LetCont (loop, loop_parameters,
                   translate_e expr loop_env
                     (fun v body_env ->
-                      Ir.LetCont (break, "_" :: break_parameters,
+                      Ir.LetCont (break, break_parameters,
                                   translate_s next break_env k ek bks cks rk,
-                        Ir.LetCont (not_taken, [], (mkcont break) body_env,
-                          Ir.LetCont (body, [], translate_s stmt body_env
-                                                    (mkcont1 loop "_") ek
-                                                    ((label, mkcont1 break) :: bks)
-                                                    ((label, mkcont1 loop) :: cks)
-                                                    rk,
-                            Ir.If (v, body, not_taken))))) ek,
-       (mkcont1 loop "_") env)
+                                  Ir.LetCont (not_taken, [], (mkcont break) body_env,
+                                    Ir.LetCont (body, [], translate_s stmt body_env
+                                                            (mkcont loop) ek
+                                                            ((label, mkcont break) :: bks)
+                                                            ((label, mkcont loop) :: cks)
+                                                            rk,
+                                      Ir.If (v, body, not_taken))))) ek,
+        (mkcont loop) env)
     | Ast.Continue (label, next) ->
-      List.assoc label cks "_" env
+      List.assoc label cks env
     | Ast.TryCatch (body, id, catch_stmt, next) ->
       let rest = gensym "rest" in
       let (rest_env, rest_parameters) = fresh_env env in
@@ -159,61 +184,61 @@ let rec translate_s stmt env k ek bks cks rk =
                     translate_s catch_stmt throw_env (mkcont rest) ek bks cks rk,
           translate_s body env (mkcont rest) (mkcont1 throw) bks cks rk))
     | Ast.TryFinally (body, finally_stmt, next) ->
-    (* Names for all the introduced continuations and parameters. *)
-    let rest = gensym "rest" in
-    let (rest_env, rest_parameters) = fresh_env env in
-    let rethrow = gensym "throw" in
-    let e = gensym "e" in
-    let (rethrow_env, rethrow_parameters) = fresh_env env in
-    let return = gensym "return" in
-    let v = gensym "v" in
-    let (return_env, return_parameters) = fresh_env env in
-    let break_names = List.map (fun _ -> gensym "break") bks in
-    let continue_names = List.map (fun _ -> gensym "continue") cks in
-    let finally = gensym "finally" in
-    let c = gensym "where" in
-    let x = gensym "what" in
-    let (finally_env, finally_parameters) = fresh_env env in
-    (* Wrap the existing continuations to go through finally. *)
-    let ek' = mkcont2 finally rethrow in
-    let bks' =
-      List.map2 (fun (label, _) name -> (label, mkcont2 finally name)) 
-        bks break_names in
-    let cks' =
-      List.map2 (fun (label, _) name -> (label, mkcont2 finally name))
-        cks continue_names in
-    let rk' = mkcont2 finally return in
-    (* Translate the body and wrap layers and layers of continuation bindings *)
-    let translated_body = translate_s body env (mkcont rest) ek' bks' cks' rk' in
-    let inner =
-      Ir.LetCont (rethrow, e :: rethrow_parameters, ek e rethrow_env,
-        Ir.LetCont (return, v :: return_parameters, rk v return_env,
-          Ir.LetCont (finally, c :: x :: finally_parameters,
-                      translate_s finally_stmt finally_env
-                        (mkcont1 c x) ek bks cks rk,
-            translated_body))) in
-    let middle =
-      List.fold_left2
-        (fun body name (_, ck) ->
-          let (env, parameters) = fresh_env env in
-          Ir.LetCont (name, "_" :: parameters, ck "_" env, body))
-        inner continue_names cks in
-    let outer =
-      List.fold_left2
-        (fun body name (_, bk) ->
-          let (env, parameters) = fresh_env env in
-          Ir.LetCont (name, "_" :: parameters, bk "_" env, body))
-        middle break_names bks in
-    Ir.LetCont (rest, rest_parameters, translate_s next rest_env k ek bks cks rk,
-      outer)
+      (* Names for all the introduced continuations and parameters. *)
+      let rest = gensym "rest" in
+      let (rest_env, rest_parameters) = fresh_env env in
+      let rethrow = gensym "throw" in
+      let e = gensym "e" in
+      let (rethrow_env, rethrow_parameters) = fresh_env env in
+      let return = gensym "return" in
+      let v = gensym "v" in
+      let (return_env, return_parameters) = fresh_env env in
+      let break_names = List.map (fun _ -> gensym "break") bks in
+      let continue_names = List.map (fun _ -> gensym "continue") cks in
+      let finally = gensym "finally" in
+      let c = gensym "where" in
+      let x = gensym "what" in
+      let (finally_env, finally_parameters) = fresh_env env in
+      (* Wrap the existing continuations to go through finally. *)
+      let ek' = mkcont2 finally rethrow in
+      let bks' =
+        List.map2 (fun (label, _) name -> (label, mkcont2 finally name "_")) 
+          bks break_names in
+      let cks' =
+        List.map2 (fun (label, _) name -> (label, mkcont2 finally name "_"))
+          cks continue_names in
+      let rk' = mkcont2 finally return in
+      (* Translate the body and wrap layers and layers of continuation bindings *)
+      let translated_body = translate_s body env (mkcont rest) ek' bks' cks' rk' in
+      let inner =
+        Ir.LetCont (rethrow, e :: rethrow_parameters, ek e rethrow_env,
+          Ir.LetCont (return, v :: return_parameters, rk v return_env,
+            Ir.LetCont (finally, c :: x :: finally_parameters,
+                        translate_s finally_stmt finally_env
+                          (mkcont1 c x) ek bks cks rk,
+              translated_body))) in
+      let middle =
+        List.fold_left2
+          (fun body name (_, ck) ->
+            let (env, parameters) = fresh_env env in
+            Ir.LetCont (name, "_" :: parameters, ck env, body))
+          inner continue_names cks in
+      let outer =
+        List.fold_left2
+          (fun body name (_, bk) ->
+            let (env, parameters) = fresh_env env in
+            Ir.LetCont (name, "_" :: parameters, bk env, body))
+          middle break_names bks in
+      Ir.LetCont (rest, rest_parameters, translate_s next rest_env k ek bks cks rk,
+        outer)
                                 
-let translate_one stmt env =
+let translate_one (stmt: Ast.statement) (env: environment): Ir.expression =
   translate_s stmt env
     (fun env -> Ir.CallCont ("return", ["null"]))
     (fun e env -> Ir.CallCont ("throw", [e]))
     [] [] (fun v env -> Ir.CallCont ("return", [v]))
 
-let translate_fun = function
+let translate_fun: (Ast.function_declaration -> Ir.function_declaration) = function
   | Ast.Sync (name, parameters, locals, body) ->
     let _ = reset_gensym () in
     let translated_body = translate_one body (initial_env parameters locals) in
