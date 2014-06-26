d9c22d172d0d120c956d99fb94e29885490851f9
headinthebox <emeijer@meijcrosoft.com>
2014-06-26 20:47:48 +0200

    A few more purposefully weird tests for async

---
+++ tests/async/awaitall.dart
@@ -0,0 +1,19 @@
+main_async() {
+
+  var tasks = [
+      constant_async(1), 
+      constant_async(2), 
+      constant_async(3)
+  ];
+
+  await(awaitall_async(tasks));
+
+}
+
+constant_async(x){ return x; }
+
+awaitall_async(tasks) {
+   for(var task in tasks) { 
+      await(task); 
+   }
+}
