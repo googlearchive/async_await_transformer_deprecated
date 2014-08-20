af57319dfed9304dcaf23962638d4c42f3330a1f
Kevin Millikin <kmillikin@kmillikin-macbookpro.roam.corp.google.com>
2014-08-20 11:32:22 -0700

    Do not name argument subexpressions unless needed.
    Argument subexpressions only need to be named if there is an await in
    an argument subexpression to their right.  To achieve this,
    responsibility for naming argument subexpressions is moved from when
    visiting the expression itself to when visiting its parent, which
    knows if there is an await to the right.
    
    Then to avoid naming values (identifiers, constants) they must be
    inspected before adding a binding.

---
+++ tests/async/awaited_arguments.dart
@@ -0,0 +1,38 @@
+f(s) {
+  return new Future(() => print(s));
+}
+
+g(x, y, z) {}
+
+test0() async {
+  g(print('a'), print('b'), print('c'));
+}
+
+test1() async {
+  g(print('a'), print('b'), await f('c'));
+}
+
+test2() async {
+  g(print('a'), await f('b'), print('c'));
+}
+
+test3() async {
+  g(print('a'), await f('b'), await f('c'));
+}
+
+test4() async {
+  g(await f('a'), print('b'), print('c'));
+}
+
+test5() async {
+  g(await f('a'), print('b'), await f('c'));
+}
+
+test6() async {
+  g(await f('a'), await f('b'), print('c'));
+}
+
+test7() async {
+  g(await f('a'), await f('b'), await f('c'));
+}
+  
