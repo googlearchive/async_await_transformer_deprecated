6a2a0b2a3ef66f9498542eade32529e89ba68fb7
Kevin Millikin <kmillikin@chromium.org>
2014-09-08 11:21:11 +0200

    Use the new compilation strategy for the transformer.
    The new compilation strategy is to only translate async bodies, and
    copy the rest of the input file unchanged to the output.
    
    Also, abstract common compilation code out of the transformer and
    standalone command line program.

--- bin/async_await.dart
+++ bin/async_await.dart
@@ -6,50 +6,18 @@
 
 import 'dart:io';
 
-import 'package:analyzer/src/generated/error.dart';
-import 'package:analyzer/src/generated/parser.dart';
-import 'package:analyzer/src/generated/scanner.dart';
-
-import 'package:async_await/src/error_collector.dart';
-import 'package:async_await/src/pretty_printer.dart';
-import 'package:async_await/src/xform.dart';
+import 'package:async_await/src/compiler.dart';
 
 main(List<String> args) {
   if (args.length != 1) {
     print('Usage: async_await.dart [file]');
     exit(0);
   }
-
-  var errorListener = new ErrorCollector();
   var source = new File(args.first).readAsStringSync();
-  var unit = _parse(source, errorListener);
-  if (errorListener.errors.isNotEmpty) {
+  var output = compile(source, (errorCollector) {
     print("Errors:");
-    for (var error in errorListener.errors) {
-      print(error);
-    }
+    errorCollector.errors.forEach(print);
     exit(1);
-  }
-
-  var worklistBuilder = new WorklistBuilder();
-  worklistBuilder.visit(unit);
-  var transform = new AsyncTransformer();
-  var pretty = new PrettyPrinter();
-  int position = 0;
-  for (var item in worklistBuilder.worklist) {
-    pretty.buffer.write(source.substring(position, item.position));
-    pretty.visit(transform.visit(item.sourceBody));
-    position = item.sourceBody.end;
-  }
-  pretty.buffer.write(source.substring(position));
-  print(pretty.buffer);
-}
-
-_parse(String source, AnalysisErrorListener errorListener) {
-  var reader = new CharSequenceReader(source);
-  var scanner = new Scanner(null, reader, errorListener);
-  var token = scanner.tokenize();
-  var parser = new Parser(null, errorListener);
-  parser.parseAsync = true;
-  return parser.parseCompilationUnit(token);
+  });
+  print(output);
 }
--- lib/async_await.dart
+++ lib/async_await.dart
@@ -2,34 +2,13 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'package:analyzer/src/generated/ast.dart';
-import 'package:analyzer/src/generated/error.dart';
-import 'package:analyzer/src/generated/parser.dart';
-import 'package:analyzer/src/generated/scanner.dart';
-
-import 'src/error_collector.dart';
-import 'src/xform.dart';
+import 'src/compiler.dart' as compiler;
 
 /// Given [source], a string of Dart code which may contain async/await syntax,
 /// compiles it down to raw Dart 1.0 syntax.
 String compile(String source) {
-  var errorListener = new ErrorCollector();
-  var unit = _parse(source, errorListener);
-
-  if (errorListener.errors.isNotEmpty) {
+  return compiler.compile(source, (errorCollector) {
     throw new FormatException(
-      "Compilation error:\n${errorListener.errors.join("\n")}");
-  }
-
-  var transform = new AsyncTransformer();
-  return transform.visit(unit).toString();
-}
-
-CompilationUnit _parse(String source, AnalysisErrorListener errorListener) {
-  var reader = new CharSequenceReader(source);
-  var scanner = new Scanner(null, reader, errorListener);
-  var token = scanner.tokenize();
-  var parser = new Parser(null, errorListener);
-  parser.parseAsync = true;
-  return parser.parseCompilationUnit(token);
+        "Compilation error:\n${errorCollector.errors.join("\n")}");
+  });
 }
